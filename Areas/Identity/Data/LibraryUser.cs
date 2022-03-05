45f347971e4af6153b5e22a53ca9166d5a47c980
MiGran4300 <96368111+MiGran4300@users.noreply.github.com>
2022-03-05 20:45:09 +0200

    Add files via upload

---
+++ Areas/Identity/Data/LibraryUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Areas.Identity.Data
+{
+    public class LibraryUser : IdentityUser
+    {
+        [PersonalData]
+        public string? Name { get; set; }
+        [PersonalData]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        public DateTime? DOB { get; set; }
+    }
+}
