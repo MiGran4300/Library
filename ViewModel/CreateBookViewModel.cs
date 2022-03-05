f439f220e1725ff9d1f988b3ae86f955a6cad318
MiGran4300 <96368111+MiGran4300@users.noreply.github.com>
2022-03-05 20:49:03 +0200

    Add files via upload

---
+++ ViewModel/CreateBookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Library.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.ViewModel
+{
+    public class CreateBookViewModel: Book
+    {
+
+        
+        public IFormFile? Photo { get; set; }
+
+    }
+}
---
+++ ViewModel/EditRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.ViewModel
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel() 
+        {
+            User = new List<string>();
+        }
+        public string? Id { get; set; }
+        [Required(ErrorMessage="Role name is required")]
+        public string? RoleName { get; set; }
+        public List<string>? User { get; set; }
+    }
+}
